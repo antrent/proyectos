9afd0ac6f385c48a230dfb948cab61bf2a8d9bb1
antonio renteria <antrent@gmail.com>
2026-08-20 17:06:36 -0500

    Se ajusta la nueva ip de la BD
    se actualiza la direccion ip de la bd

--- app.becasual/backend/src/config/db.js
+++ app.becasual/backend/src/config/db.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 // Asegurar que en desarrollo local no apuntemos a la IP remota por error, redirigiendo a localhost
 // IMPORTANTE: Se valida !process.env.K_SERVICE para asegurar que no estamos en Cloud Run (producción)
-if (!process.env.K_SERVICE && process.env.DATABASE_URL && (process.env.DATABASE_URL.includes('136.112.170.11') || process.env.DATABASE_URL.includes('136.112.'))) {
+if (!process.env.K_SERVICE && process.env.DATABASE_URL && (process.env.DATABASE_URL.includes('136.112.170.11') || process.env.DATABASE_URL.includes('34.63.207.75'))) {
   console.log('🛡️ [Prisma] Detectada URL de base de datos remota en entorno local.');
   console.log('Redirigiendo conexión a PostgreSQL local (localhost:5432) por política Local-First.');
   process.env.DATABASE_URL = 'postgresql://becasual_user:becasual_password_987@localhost:5432/becasual?schema=public';
